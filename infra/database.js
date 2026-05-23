c2395f699d96bba81e477027d1beabfc69f3100a
Blzvini <lopes.viniciusm99@gmail.com>
2026-05-23 18:06:11 -0300

    fix: standardize SSL configuration for database connection

--- infra/database.js
+++ infra/database.js
@@ -7,12 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl:
-      process.env.NODE_ENV === "development"
-        ? false
-        : {
-            rejectUnauthorized: false,
-          },
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
   try {
