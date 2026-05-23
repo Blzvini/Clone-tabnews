d02c8e5763b6e05da62fc04dd3d7476440c137ce
Blzvini <lopes.viniciusm99@gmail.com>
2026-05-23 14:34:32 -0300

    Finish endpint /status/

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,42 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1+1 AS sum;");
-  console.log(result.rows);
-  res.status(200).json({ status: "Não" });
+  const updatedAt = new Date().toISOString();
+
+  /* Query para obter a versão do Postgres */
+  const versionPostgres = await database.query("SELECT version();");
+  const parsedVersionPostgres = versionPostgres.rows[0].version.split(" ")[1];
+
+  /* Query para obter o número máximo de conexões permitidas */
+  const maxConnections = await database.query("SHOW max_connections;");
+  const parsedMaxConnections = maxConnections.rows[0].max_connections
+    .split(",")[0]
+    .trim();
+
+  /* Query pra trazer o número de conexões ativas */
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const activeConnectionsValue = activeConnections.rows[0].count;
+
+  console.log(
+    parsedVersionPostgres,
+    parsedMaxConnections,
+    activeConnectionsValue,
+  );
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version_postgres: parsedVersionPostgres,
+        max_connections: parsedMaxConnections,
+        active_connections: activeConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,7 +4,65 @@
   Então o status da resposta deve ser 200
 */
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  test("GET to /api/v1/status should return 200", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    expect(responseBody.updated_at).toBeDefined();
+
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  });
+
+  test("GET to /api/v1/status should return version_postgres", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+
+    expect(responseBody.dependencies.database.version_postgres).toBeDefined();
+
+    const version = responseBody.dependencies.database.version_postgres;
+    expect(version).toBe("16.14");
+  });
+
+  test("GET to /api/v1/status should return max_connections", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    expect(responseBody.dependencies.database.max_connections).toBeDefined();
+
+    const maxConnections = responseBody.dependencies.database.max_connections;
+    expect(maxConnections).toBe("100");
+  });
+
+  test("GET to /api/v1/status should return active_connections", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    expect(responseBody.dependencies.database.active_connections).toBeDefined();
+    const activeConnections =
+      responseBody.dependencies.database.active_connections;
+    expect(activeConnections).toBe(1);
+  });
+
+  test("GET to /api/v1/status should return only required fields", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+
+    // Verificar que existem apenas os campos específicos e que não há campos extras
+    expect(Object.keys(responseBody)).toEqual(["updated_at", "dependencies"]);
+    expect(Object.keys(responseBody.dependencies)).toEqual(["database"]);
+    expect(Object.keys(responseBody.dependencies.database)).toEqual([
+      "version_postgres",
+      "max_connections",
+      "active_connections",
+    ]);
+  });
 });
